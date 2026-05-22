d000d96d02997126896055a188ab89d620a33052
Jakar79 <karim.jabri@gmail.com>
2026-05-22 16:49:33 +0100

    feat(serializers): add update method and remove debug print statements
    - Implement `update()` method in `FormSerializer` to handle nested
      updates for sections, fields, and choices
    - Support creating, updating, and deleting nested objects during
      form updates by diffing existing vs incoming data
    - Remove debug `print` statements from `create()` method

--- backend/apps/form_builder/serializers.py
+++ backend/apps/form_builder/serializers.py
@@ -33,10 +33,6 @@ class FormSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
 
-        print(" validated_data", validated_data)
-        print(" validated_data keys", validated_data.keys())
-        print(" validated_data values", validated_data.values())
-
         # :extracting sections and fields from validated_data
         sections_data = validated_data.pop('sections', [])
         # :creating and saving form instance
@@ -63,3 +59,85 @@ class FormSerializer(serializers.ModelSerializer):
                 for choice_data in choices_data:
                     FieldChoice.objects.create(field=field, **choice_data)
         return form
+
+def update(self, instance, validated_data):
+    sections_data = validated_data.pop('sections', [])
+
+    # :get existing sections and fields from validated_data
+    existing_sections = instance.sections.all()
+    existing_section = list(existing_sections)
+    # existing_section_ids = [section.id for section in existing_sections]
+
+    # :save form instance
+    instance.name = validated_data.get('name', instance.name)
+    instance.save()
+
+    # :looping through sections data and updating them
+    for section_data in sections_data:
+
+        if section_id := section_data.get('id'):
+            # :Updating existing section
+            section = existing_section.pop(0)
+            section.name = section_data.get('name', section.name)
+            section.save()
+        else:
+            # :creating new section
+            section = Section.objects.create(
+                name=section_data.get('name'), 
+                form=instance)
+
+        # :getting the form fields 
+        fields_data = section_data.pop("form_fields", [])
+
+        # :getting the existing fields
+        existing_fields = section.fields.all()
+        existing_fields = list(existing_fields)
+
+        # :looping through fields data
+        for field_data in fields_data:
+            if field_id := field_data.get("id", None):
+                # :field exists, update it
+                field = existing_fields.pop(0)
+                field.label = field_data.get('label', field.label)
+                field.field_type = field_data.get('field_type', field.field_type)
+                field.save()
+            else:
+                # :field does not exist, create it
+                Field.objects.create(
+                    label=field_data.get('label'),
+                    field_type=field_data.get('field_type'),
+                    section=section)
+
+            # :get the choices data
+            choices_data = field_data.get('choices', [])
+
+            # :existing choices
+            existing_choices = field.choices.all()
+            existing_choices = list(existing_choices)
+
+            # :looping through choices data
+            for choice_data in choices_data:
+                if choice_id := choice_data.get("id", None):
+                    # :choice exists, update it
+                    choice = existing_choices.pop(0)
+                    choice.choice_text = choice_data.get(
+                        'choice_text', choice.choice_text)
+                    choice.save()
+                else:
+                    # :choice does not exist, create it
+                    choice =FieldChoice.objects.create(
+                        choice_text=choice_data.get('choice_text'),
+                        field=field)
+
+            # :delete remaining choices
+            for choice in existing_choices:
+                choice.delete()
+        # :delete remaining fields
+        for field in existing_fields:
+            field.delete()
+    # :delete remaining sections
+    for section in existing_section:
+        section.delete()
+
+
+    return instance
